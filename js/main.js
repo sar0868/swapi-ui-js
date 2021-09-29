0469c5cf7d829e5133401d644c2ad7e816123a9b
sar0868 <safarov-ar@yandex.ru>
2021-09-29 19:46:05 +0300

    stepic 4.1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-$(function(){
+$(function() {
     var includes = $('[data-include]');
     //document.querySelector("[data-include]").innerText;
-    jQuery.each(includes, function(){
+    jQuery.each(includes, function() {
         var file = 'components/' + $(this).data('include') + '.html';
         $(this).load(file);
     });
+});
+$('.menu-btn').on('click', function() {
+    $('.menu-wrapper').slideToggle('slow');
 });
